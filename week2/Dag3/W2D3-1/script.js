3625f1ad1642d9e57ff03a8d2a8a1888b3c90f21
Geeske Boer <gboer89@gmail.com>
2020-02-12 10:48:24 +0100

    Toevoegen opdracht:BasicArrowFunctions

---
+++ week2/Dag3/W2D3-1/script.js
@@ -0,0 +1,24 @@
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = function() {
+  console.log("Joe, ik rock de arrow functions!");
+};
+ikRockArrowFunctions();
+
+const ikRockArrowFunctions = () => {
+  console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+const fivePlusSeven = function() {
+  return 5 + 7;
+};
+fivePlusSeven();
+
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+() => 1 + 2;
+const myFunction = (a, b) => a + b;
+const addFive = a => a + 5;
+const createObject = () => ({ greeting: "hoi" });
