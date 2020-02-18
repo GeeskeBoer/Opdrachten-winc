286b1b4a22d2f2b6ae81c49e04095946ed47d35b
Geeske Boer <gboer89@gmail.com>
2020-02-18 12:10:24 +0100

    API key met POSTman

--- week3/dag2/W3D2-2/script.js
+++ week3/dag2/W3D2-2/script.js
@@ -9,3 +9,15 @@ const klaarMetHuiswerk = () => {
   console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk!`);
 };
 huiswerkMaken("wiskunde", klaarMetHuiswerk);
+
+const testNum = () => {
+  return new Promise((resolve, reject) => {
+    if (testNum < 10) {
+      return "kleiner dan 10";
+    } else {
+      reject("10+");
+    }
+  });
+};
+
+testNum(12);
